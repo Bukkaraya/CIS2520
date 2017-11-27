57e3fcf68c4242c71dba2584ff129c2289e4ef53
Abinav <Abinavbukkaraya@gmail.com>
2017-11-27 09:41:27 -0500

    Finished file reading part.

--- Assignment4/src/main.c
+++ Assignment4/src/main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "binarySearchTreeAPI.h"
 #include "rule.h"
 #define MAX_LEN 400
 
-
+void removeNewLine(char* str);
 
 
 int main(int argc, char** argv){
     FILE* inputFile = NULL;
     char fileName[MAX_LEN];
     char fileValues[MAX_LEN];
+    char secondValue[MAX_LEN];
     Tree *wordTree = createBinTree(compareRules, deleteRule);
 
 
@@ -28,8 +30,19 @@ int main(int argc, char** argv){
 
     printf("Successfully opened the file.\n");
 
-    while(fgets)
+    while(fgets(fileValues, MAX_LEN, inputFile)){
+        fgets(secondValue, MAX_LEN, inputFile);
+        printf("%s%s\n", fileValues, secondValue);
+    }
 
 
     return 0;
+}
+
+
+
+void removeNewLine(char* str){
+    if(str[strlen(str) - 1] == '\n'){
+        str[strlen(str) - 1] = '\0';
+    }
 }
